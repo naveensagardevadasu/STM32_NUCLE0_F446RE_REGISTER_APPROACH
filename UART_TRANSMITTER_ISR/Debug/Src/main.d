9d61e0fda14e31fb46762aa176693c242a8a70b9
naveen_sagar_devadasu <105536708+naveensagardevadasu@users.noreply.github.com>
2024-06-16 23:16:37 +0530

    USART polling mode modified

--- UART_TRANSMITTER_ISR/Debug/Src/main.d
+++ UART_TRANSMITTER_ISR/Debug/Src/main.d
@@ -1,10 +1,9 @@
 Src/main.o: ../Src/main.c ../Src/stm32f446xx.h ../Src/core_cm4.h \
  ../Src/cmsis_version.h ../Src/cmsis_compiler.h ../Src/cmsis_gcc.h \
- ../Src/mpu_armv7.h ../Src/system_stm32f4xx.h
+ ../Src/mpu_armv7.h
 ../Src/stm32f446xx.h:
 ../Src/core_cm4.h:
 ../Src/cmsis_version.h:
 ../Src/cmsis_compiler.h:
 ../Src/cmsis_gcc.h:
 ../Src/mpu_armv7.h:
-../Src/system_stm32f4xx.h:
